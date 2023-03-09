6cd0f5f93da170abfe219a49ed8aa33634f2d482
Sergey Gurvich <gurvich_sergey@solarturbines.com>
2023-03-08 17:01:00 -0800

    changes

--- main.py
+++ main.py
@@ -9,20 +9,43 @@ app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.read_csv('grouped.csv')
+# df_ = px.data.gapminder().query("year==2007")
+fig = px.scatter_geo(df, lat='lat',lon='lon',
+                     hover_name="unit", size="count",
+                     scope='usa',
+                     center={'lat':37.16611, 'lon':-119.44944},
+                     # fitbounds='locations',
+                     # animation_group="year",
+                     animation_frame="year",
+                     color_continuous_scale=px.colors.cyclical.IceFire,
+                     color='count'
+                     )
+fig.update_geos(
+    resolution=50,
+    showcoastlines=True, coastlinecolor="RebeccaPurple",
+    showland=True, landcolor="Green",
+    showocean=True, oceancolor="LightBlue",
+    showlakes=True, lakecolor="Blue",
+    showrivers=True, rivercolor="Blue",
+    showcountries=True, countrycolor="Black",
+    showsubunits=True, subunitcolor="Orange"
+)
+fig.update_geos(fitbounds="locations")
+fig.update_layout(height=300, margin={"r":0,"t":0,"l":0,"b":0})
+fig.update_layout(
+    mapbox = {
+        'style': "stamen-terrain",
+        'center': {'lon': -73, 'lat': 46 },
+        'zoom': 5},
+    showlegend = False)
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig.update_layout(height=500, margin={"r":0,"t":0,"l":0,"b":0})
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='California Wildfires Analysis'),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+    html.Div(children='Wildfires Geography Visualization'),
 
     dcc.Graph(
         id='example-graph',
