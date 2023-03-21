3486602521db86b4ef22373cd313f0df34a551ca
Sergey Gurvich <sgurvich@gmail.com>
2023-03-20 20:42:38 -0700

    more

--- web_app.py
+++ web_app.py
@@ -7,38 +7,49 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 app = Dash(__name__)
 
+# Load Data
+df_1 = pd.read_csv('wildfires_grouped_task1.csv')
+df_2 = pd.read_csv('wildfires_grouped_task2_2.csv')
+df_3 = pd.read_csv('wildfires_grouped_task3.csv')
+df_4 = pd.read_csv('wildfires_grouped_task4.csv')
+df_5 = pd.read_csv('wildfires_grouped_task5.csv')
+
+df_4_1 = df_4.groupby('month').sum(numeric_only=True)
+df_4_1.reset_index(inplace=True)
+
+order = df_4.groupby('cause').sum().sort_values('count', ascending=False)
+order_list = order.index.to_list()
+
+
 def make_fig1():
-    # Task 1
-    # plot bubble map
-    df_1 = pd.read_csv('wildfires_grouped_task1.csv')
-    # df_ = px.data.gapminder().query("year==2007")
+    """
+    Function to make Task 1 - Bubble map
+    """
     fig_1 = px.scatter_geo(df_1, lat='lat',lon='lon',
                          size="count",
                          scope='usa',
                          center={'lat':37.16611, 'lon':-119.44944},
-                         # fitbounds='locations',
-                         # animation_group="year",
                          animation_frame="year",
-                         color_continuous_scale=px.colors.cyclical.IceFire,
+                         color_continuous_scale='bluered',
                          color='count',
                          hover_data=['year', 'count'],
                          hover_name="unit",
-                         # title=' Wildfires Geography over Time'
                          )
     fig_1.update_geos(
         resolution=50,
         showcoastlines=True, coastlinecolor="RebeccaPurple",
-        showland=True, landcolor="Green",
-        showocean=True, oceancolor="LightBlue",
-        showlakes=True, lakecolor="Blue",
-        showrivers=True, rivercolor="Blue",
+        showland=True, landcolor="#FFF3CD",
+        showocean=True, oceancolor="lightblue",
+        showlakes=True, lakecolor="lightblue",
+        showrivers=True, rivercolor="lightblue",
         showcountries=True, countrycolor="Black",
-        showsubunits=True, subunitcolor="Orange"
+        showsubunits=True, subunitcolor="Orange",
+        projection_scale=2.7,
     )
-    # fig.update_geos(fitbounds="locations")
     fig_1.update_layout(height=300, margin={"r":0,"t":0,"l":0,"b":0})
     fig_1.update_layout(
         mapbox = {
@@ -46,61 +57,69 @@ def make_fig1():
             'center': {'lon': -73, 'lat': 46 },
             'zoom': 5},
         showlegend = False)
-
     fig_1.update_layout(height=500, margin={"r":0,"t":0,"l":0,"b":0})
     
     return fig_1
 
 def make_fig2():
-    # TASK 2
-    # Read
-    df_2 = pd.read_csv('wildfires_grouped_task2_2.csv')
+    """
+    Function to make Task 2 - Trends
+    """
 
     # Create figure with secondary y-axis
     fig_2 = make_subplots(specs=[[{"secondary_y": True}]])
 
     # Add traces
-    fig_2.add_trace(
-        go.Scatter(x=df_2['year'].astype(str), y=df_2['count'], name="wildfires count"),
-        secondary_y=False,
-    )
+    fig_2.add_trace(go.Scatter(x=df_2['year'].astype(str), y=df_2['count'], name="wildfires count"), secondary_y=False)
+    
+    # trend line 1
+    X = df_2[['year']]
+    y=df_2['count']
+    model = LinearRegression().fit(X,y)
+    y_hat = model.predict(X)
+    
+    fig_2.add_trace(go.Scatter(x=df_2['year'].astype(str), y=y_hat, line = dict(color='royalblue', width=1, dash='dash'),
+                               name="wildfires count trend"), 
+                               secondary_y=False)
 
     fig_2.add_trace(
-        go.Scatter(x=df_2['year'].astype(str), y=df_2['area_burned'], name="wildfires area burned"),
-        secondary_y=True,
-    )
+        go.Scatter(x=df_2['year'].astype(str), y=df_2['area_burned'], name="wildfires area burned", line = dict(color='red')), 
+        secondary_y=True)
+    
+    # trend line 2
+    y=df_2['area_burned']
+    model = LinearRegression().fit(X,y)
+    y_hat2 = model.predict(X)
+    
+    fig_2.add_trace(go.Scatter(x=df_2['year'].astype(str), y=y_hat2, name="wildfires area burned trend",
+                              line = dict(color='firebrick', width=1, dash='dot')), secondary_y=True)
+    
 
-    # # Add figure title
-    # fig_2.update_layout(
-    #     title_text="Wildfires Trends"
-    # )
 
     # Set x-axis title
     fig_2.update_xaxes(title_text="year")
 
     # Set y-axes titles
-    fig_2.update_yaxes(title_text="<b>wildfires</b> count", secondary_y=False)
-    fig_2.update_yaxes(title_text="<b>wildfires</b> area burned", secondary_y=True)
+    fig_2.update_yaxes(title_text="wildfires count", secondary_y=False)
+    fig_2.update_yaxes(title_text="wildfires area burned", secondary_y=True)
 
     return fig_2
 
 def make_fig3():
-    # TASK 3
-    df_3 = pd.read_csv('wildfires_grouped_task3.csv')
-    fig_3 = px.bar(df_3, x="year", y="count", color="cause")
+    """
+    Function to make Task 3 - Onset Stacked Bar Chart
+    """
+
+    fig_3 = px.bar(df_3, x="year", y="count", color="cause", category_orders={'cause': order_list})
+    fig_3.update_yaxes(title_text="wildfires count")
     
     return fig_3
 
-df_4 = pd.read_csv('wildfires_grouped_task4.csv')
-df_4_1 = df_4.groupby('month').sum()
-df_4_1.reset_index(inplace=True)
-order = df_4.groupby('cause').sum().sort_values('count', ascending=False)
-order_list = order.index.to_list()
+
 def make_fig4():
-    # TASK 4
-
-
-
+    """
+    Function to make Task 4.1 - Seasonality Bar Charts
+    """
     fig_4 = px.bar(df_4_1, x="month", y="count",
                 height=500,
                 width=1000,
@@ -116,6 +135,9 @@ def make_fig4():
 
 
 def make_fig5():
+    """
+    Function to make Task 4.2 - Seasonality Bar Charts
+    """
     fig_5 = px.bar(df_4, x="month", y="count", 
                 # color="cause",
                 facet_row='cause', 
@@ -136,13 +158,17 @@ def make_fig5():
     fig_5.for_each_annotation(lambda a: a.update(text=a.text.split("- ")[1].split("/")[0]))
     fig_5.update_yaxes(matches=None)
 
-    # TASK 5
-
 
     return fig_5
-df_5 = pd.read_csv('wildfires_grouped_task5.csv')
+
 def make_fig6():
-    fig_6 = px.area(df_5, x="year", y="duration")
+    """
+    Function to make Task 5 - Avg Fire Duration, Area Chart
+    """
+    fig_6 = px.area(df_5, x="year", y="duration",
+                height=500,
+                width=1000)
+    fig_6.update_yaxes(title_text="average fire duration, days")
     return fig_6
 
 
@@ -150,109 +176,50 @@ def make_fig6():
 
 
 app.layout = html.Div(
-    style={
-# "background-image": "url('/assets/fire.jpeg')",
-"background-color": "light-grey",
-"background-repeat": "repeat",
-"background-position": "right top",
-"background-size": "150px 100px"
-},
+
     children=[
-    html.Button(id='b_t1', n_clicks=0, children='Task1'),
-    html.Button(id='b_t2', n_clicks=0, children='Task2'),
-    html.Button(id='b_t3', n_clicks=0, children='Task3'),
-    html.Button(id='b_t4', n_clicks=0, children='Task4'),
-    html.Button(id='b_t5', n_clicks=0, children='Task5'),
-    html.Button(id='b_t6', n_clicks=0, children='Task6'),
-        
-        
-    # html.H1(children='California Wildfires Analysis'),
-
-    # html.H2(children='Task 1: Wildfires Geography'),
-    dcc.Graph(
-        id='fig_1',
-        figure=make_fig1()
-    ),
-    
-    
-#     html.H2(children='Task 2: Wildfires Trends'),
-#     dcc.Graph(
-#         id='fig_2',
-#         figure=make_fig2()
-#     ),
-    
-#     html.H2(children='Task 3: Causes Of Wildfires Onset'),
-#     dcc.Graph(
-#         id='fig_3',
-#         figure=fig_3
-#     ),
-    
-#     html.H2(children='Task 4.1: Seasonality Of Wildfires'),
-#     dcc.Graph(
-#         id='fig_4',
-#         figure=fig_4
-#     ),
-
-
-#     html.H2(children="Task 4.2: Seasonality Of Wildfires by Their Cause"),
-#     dcc.Graph(
-#         id='fig_5',
-#         figure=fig_5
-#     ),
-    
-#     html.H2(children="Task 5: Fire Average Duration"),
-#     dcc.Graph(
-#         id='fig_6',
-#         figure=fig_6
-#     ),
-    
-]
-                     
-                     
+            html.H1(children='California Wildfires Analysis'),
+            dcc.Dropdown(   options=[
+               {'label': 'Task 1: Wildfires Geography', 'value': 'task1'},
+               {'label': 'Task 2: Wildfires Trends', 'value': 'task2'},
+               {'label': 'Task 3: Causes Of Wildfires Onset', 'value': 'task3'},
+               {'label': 'Task 4.1: Seasonality Of Wildfires by Years', 'value': 'task4'},
+               {'label': 'Task 4.2: Seasonality Of Wildfires by Their Cause', 'value': 'task5'},
+               {'label': 'Task 5: Average Wildfire Duration', 'value': 'task6'},
+               ],
+               value='task1',id='demo-dropdown', searchable=False, clearable=False),
+            html.Div(id='dd-output-container'),
+            dcc.Graph(id='fig_1', figure=make_fig1()),
+    ]
 )
+    
 
 
-@app.callback([Output('fig_1', 'figure'),
-              Output('b_t1', 'n_clicks'),
-              Output('b_t2', 'n_clicks'),
-              Output('b_t3', 'n_clicks'),
-              Output('b_t4', 'n_clicks'),
-              Output('b_t5', 'n_clicks'),
-              Output('b_t6', 'n_clicks'),
-              ],
-              Input('b_t1', 'n_clicks'),
-              Input('b_t2', 'n_clicks'),
-              Input('b_t3', 'n_clicks'),
-              Input('b_t4', 'n_clicks'),
-              Input('b_t5', 'n_clicks'),
-              Input('b_t6', 'n_clicks'),
-              prevent_initial_call=True)
-def update_graph(
-                button1, 
-                button2,
-                button3, 
-                button4,
-                button5, 
-                button6,
-                ):
-    if button1:
+    
+@app.callback(
+    Output('fig_1', 'figure'),
+    Input('demo-dropdown', 'value')
+)
+def update_output(value):
+    # return f'You have selected {value}'
+    if value == 'task1':
         fig = make_fig1()
-        return fig, 0, 0, 0, 0, 0, 0
-    if button2:
+        return fig
+    if value == 'task2':
         fig = make_fig2()
-        return fig, 0, 0, 0, 0, 0, 0
-    if button3:
+        return fig
+    if value == 'task3':
         fig = make_fig3()
-        return fig, 0, 0, 0, 0, 0, 0
-    if button4:
+        return fig
+    if value == 'task4':
         fig = make_fig4()
-        return fig, 0, 0, 0, 0, 0, 0
-    if button5:
+        return fig
+    if value == 'task5':
         fig = make_fig5()
-        return fig, 0, 0, 0, 0, 0, 0
-    if button6:
+        return fig
+    if value == 'task6':
         fig = make_fig6()
-        return fig, 0, 0, 0, 0, 0, 0
+        return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
